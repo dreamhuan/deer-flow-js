8eb923b8b5c92d0d83618ffd1245c1ba01d05b40
fukaiqi <fu_kaiqi@qq.com>
2025-09-29 18:07:59 +0800

    feat: tavily

--- src/cli.ts
+++ src/cli.ts
@@ -52,3 +52,5 @@ async function cli() {
 }
 
 cli();
+
+
--- src/tools.ts
+++ src/tools.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod/v4';
 import { tool } from '@langchain/core/tools';
+import { TavilyCrawl, TavilySearch } from '@langchain/tavily';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Define tools
 const add = tool(({ a, b }) => a + b, {
@@ -38,6 +42,23 @@ const divide = tool(({ a, b }) => a / b, {
   }),
 });
 
-export const tools = [add, subtract, multiply, divide];
+const tavilySearchTool = new TavilySearch({
+  maxResults: 5,
+});
+
+const tavilyCrawlTool = new TavilyCrawl({
+  maxDepth: 3,
+  maxBreadth: 50,
+});
+
+// https://docs.langchain.com/oss/javascript/integrations/tools/index#tools-and-toolkits
+export const tools = [
+  add,
+  subtract,
+  multiply,
+  divide,
+  tavilySearchTool,
+  tavilyCrawlTool,
+];
 
 export const toolsByName = Object.fromEntries(tools.map((t) => [t.name, t]));
